730c67836d13bd9a0575d274c7d7a588c4e022b8
Alex Emelyanov <alex84_84_84@mail.ru>
2022-01-18 11:03:28 +0300

    try append path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ import sys
 #sys.path.insert(0, os.path.abspath('./repo'))
 #sys.path.insert(0, os.path.abspath('../../supervisely_lib'))
 #sys.path.insert(0, '/app')
-sys.path.insert(0, os.path.abspath('/toy_code'))
+sys.path.append(0, os.path.abspath('../../toy_code'))
 
 
 # -- Project information -----------------------------------------------------
